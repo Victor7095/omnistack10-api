2f65266eeab01eaadecfd06b3415d5774efa838d
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-01-17 00:33:23 -0400

    Add Dev

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,39 @@
 const { Router } = require("express");
+const axios = require("axios");
+const Dev = require("./models/Dev");
 
 const routes = Router();
 
-app.get("/", (req, res)=> {
+routes.get("/", (req, res)=> {
     return res.json({
         "message":"Hello wrlda"
     });
 });
 
-app.get("/", (req, res)=> {
+routes.get("/s", (req, res)=> {
     return res.json({
         "message":"Hello wrlda"
     });
 });
 
-app.post("/", (req, res)=> {
-    return res.json({
-        "message":"Hello wrlda"
+routes.post("/devs", async (req, res)=> {
+    const { github_username, techs } = req.body;
+
+    const apiResponse = await axios.get(`https://api.github/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = apiResponse.data;
+
+    const techsArray = techs.split(",").map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray
     });
+
+    return res.json(dev);
 });
 
 module.exports = routes;
