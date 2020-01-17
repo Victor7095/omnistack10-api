0bf73fa42d9f4ac56bedac06dd9093cd7e3335e6
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-01-17 02:02:25 -0400

    search query

--- src/routes.js
+++ src/routes.js
@@ -1,45 +1,13 @@
 const { Router } = require("express");
-const axios = require("axios");
-const Dev = require("./models/Dev");
+const DevController = require("./controllers/DevController");
+const SearchController = require("./controllers/SearchController");
 
 const routes = Router();
 
-routes.get("/", (req, res)=> {
-    return res.json({
-        "message":"Hello wrlda"
-    });
-});
+routes.get("/devs", DevController.index);
 
-routes.get("/s", (req, res)=> {
-    return res.json({
-        "message":"Hello wrlda"
-    });
-});
+routes.get("/search", SearchController.index);
 
-routes.post("/devs", async (req, res)=> {
-    const { github_username, techs, latitude, longitude } = req.body;
-
-    const apiResponse = await axios.get(`https://api.github/users/${github_username}`);
-
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    const techsArray = techs.split(",").map(tech => tech.trim());
-
-    const location = { 
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location
-    });
-
-    return res.json(dev);
-});
+routes.post("/devs", DevController.store);
 
 module.exports = routes;
