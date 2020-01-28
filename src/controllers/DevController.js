29bf9db62980b27e95797ff17922df0371fe28b2
Victor <vypl.snf19@uea.edu.br>
2020-01-28 12:48:30 -0400

    Socket

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
     async index(req,res) {
@@ -37,6 +38,10 @@ module.exports = {
             techs: techsArray,
             location
         });
+        
+        const coordinates = { latitude, longitude };
+        const sendSocketMessageTo = findConnections(coordinates, techsArray);
+        sendMessage(sendSocketMessageTo, "new-dev", dev);
 
         return res.json(dev);
     },
--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -2,25 +2,25 @@ const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
-    async index(req,res) {
-        const { latitude, longitude, techs } = req.query;
+  async index(req,res) {
+    const { latitude, longitude, techs } = req.query;
 
-        const techsArray = parseStringAsArray(techs);
+    const techsArray = parseStringAsArray(techs);
 
-        const devs = await Dev.find({
-            techs: {
-                $in: techsArray
-            },
-            location: {
-                $near: {
-                    $geometry: {
-                        type: "Point",
-                        coordinates: [longitude, latitude]
-                    },
-                    $maxDistance: 10000
-                }
-            }
-        });
-        return res.json(devs);
-    }
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        }
+      }
+    });
+    return res.json(devs);
+  }
 };
